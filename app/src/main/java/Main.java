322630c9131f047de3c69183f36663aa6ae57062
lucan8 <gamer.lucan72@gmail.com>
2025-05-25 11:51:42 +0300

    Now also initilazes the app

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,10 +1,12 @@
 import services.Service;
 import db_conn.DBConn;
+import app_init.AppInitializer;
 import java.sql.SQLException;
 public class Main{
     public static void main(String[] args) {
         try{
             DBConn.TestConn();
+            AppInitializer.init();
         } catch (SQLException e){
             System.err.println("[ERROR] Testing the database connection: " + e.getMessage());
             System.exit(1);
