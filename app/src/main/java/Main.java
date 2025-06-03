58cb95d6856b085152c578253c8fd7e418935ef8
lucan8 <gamer.lucan72@gmail.com>
2025-06-03 09:59:51 +0300

    Using auth menu before main menu

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,7 +1,7 @@
-import services.Service;
-import db_conn.DBConn;
-import app_init.AppInitializer;
 import java.sql.SQLException;
+
+import app_init.AppInitializer;
+import db_conn.DBConn;
 public class Main{
     public static void main(String[] args) {
         try{
@@ -11,7 +11,16 @@ public class Main{
             System.err.println("[ERROR] Testing the database connection: " + e.getMessage());
             System.exit(1);
         }
+
+        System.out.println("Welcome to the application!");
+        System.out.println("Please log in or register to continue.");
+        // Authenticate the user before showing the main menu
+        while (AppInitializer.getAuthService().isAuthenticated == false)
+            AppInitializer.getAuthService().menu();
+        
+        System.out.println("Main menu");
+        // Show the main menu until the user exits
         while(true)
-            Service.menu();
+            AppInitializer.getAppService().menu();
     }
 }
